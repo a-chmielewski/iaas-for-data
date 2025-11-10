d4b6c269bd280a06a4443f7b8f7eb5f092e51813
a-chmielewski <chmielaq@gmail.com>
2025-11-10 14:23:14 +0100

    Format Terraform output blocks for consistency and readability.

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
-output "run_url" { value = module.cloud_run.url }
-output "bucket_name" { value = module.storage.name }
-output "bigquery_dataset" { value = module.bigquery.dataset_id }
+output "run_url" {
+  value = module.cloud_run.url
+}
+
+output "bucket_name" {
+  value = module.storage.name
+}
+
+output "bigquery_dataset" {
+  value = module.bigquery.dataset_id
+}
